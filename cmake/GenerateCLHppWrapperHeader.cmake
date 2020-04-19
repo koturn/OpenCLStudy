4fbf2397c97d6e507cd5967c940a6f9f0cc9f35f
koturn <jeak.koutan.apple@gmail.com>
2020-04-19 13:26:10 +0900

    Add generating macro definition

--- cmake/GenerateCLHppWrapperHeader.cmake
+++ cmake/GenerateCLHppWrapperHeader.cmake
@@ -6,9 +6,22 @@ function(generate_clhpp_wrapper_header header_file_path)
   set(options)
   set(oneValueArgs
     HEADER_VERSION
-    ENABLE_EXCEPTIONS
     MINIMUM_OPENCL_VERSION
     TARGET_OPENCL_VERSION
+    ENABLE_EXCEPTIONS
+    NO_STD_STRING
+    NO_STD_VECTOR
+    NO_STD_ARRAY
+    NO_STD_UNIQUE_PTR
+    ENABLE_SIZE_T_COMPATIBILITY
+    ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY
+    CL_1_2_DEFAULT_BUILD
+    USE_CL_DEVICE_FISSION
+    USE_CL_SUB_GROUPS_KHR
+    USE_CL_IMAGE2D_FROM_BUFFER_KHR
+    USE_DX_INTEROP
+    USE_IL_KHR
+    USER_OVERRIDE_ERROR_STRINGS
     SILENCE_DEPRECATION)
   cmake_parse_arguments(CL_HPP "${options}" "${oneValueArgs}" "" ${ARGN})
 
@@ -46,6 +59,22 @@ function(generate_clhpp_wrapper_header header_file_path)
     if(CL_HPP_MINIMUM_OPENCL_VERSION LESS_EQUAL 220)
       set(CL_USE_DEPRECATED_OPENCL_2_2_APIS ON)
     endif()
+    if(CL_HPP_NO_STD_STRING)
+      set(__NO_STD_STRING ON)
+    endif()
+    if(CL_HPP_NO_STD_VECTOR)
+      set(__NO_STD_VECTOR ON)
+    endif()
+    if(CL_HPP_USER_OVERRIDE_ERROR_STRINGS)
+      set(__CL_USER_OVERRIDE_ERROR_STRINGS ON)
+    endif()
+    if(CL_HPP_USE_CL_DEVICE_FISSION)
+      set(USE_CL_DEVICE_FISSION ON)
+    endif()
+    if(CL_HPP_USE_DX_INTEROP)
+      set(USE_DX_INTEROP ON)
+    endif()
+
     configure_file(
       ${CL_HPP_CURRENT_LIST_DIR}/templates/clWrapper.hpp.in
       ${header_file_path}
